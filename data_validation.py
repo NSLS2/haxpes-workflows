912db28bb51ed8649079e6dff708d6bc4c9407a1
Abigail Giles <agiles@bnl.gov>
2026-03-30 16:24:53 -0400

    export and validation code cleanup

--- data_validation.py
+++ data_validation.py
@@ -11,8 +11,8 @@ def read_stream(run, stream):
     return stream_data
 
 
-@task(retries=2, retry_delay_seconds=10)
-def read_all_streams(uid, api_key=None):
+@flow(retries=2, retry_delay_seconds=10)
+def data_validation(uid, api_key=None):
     logger = get_run_logger()
     run = get_run(uid, api_key=api_key)
     logger.info(f"Validating uid {uid}")
@@ -26,8 +26,3 @@ def read_all_streams(uid, api_key=None):
         logger.info(f"{stream} nbytes = {stream_data.nbytes:_}")
     elapsed_time = time.monotonic() - start_time
     logger.info(f"{elapsed_time = }")
-
-
-@flow
-def data_validation(uid, api_key=None):
-    read_all_streams(uid, api_key=api_key)
--- general_exporter.py
+++ general_exporter.py
@@ -11,6 +11,7 @@ from file_exporter import (
 )
 
 
+@flow
 def export_switchboard(uid, api_key=None, dry_run=False):
     logger = get_run_logger()
     run = get_run(uid, api_key=api_key)
@@ -19,41 +20,16 @@ def export_switchboard(uid, api_key=None, dry_run=False):
             if "scantype" in run.start.keys():
                 if run.start["scantype"] == "xps":
                     if run.start["analyzer_type"] == "peak":
-                        peak_export(uid, api_key=api_key, dry_run=dry_run)
+                        export_peak_xps(uid, api_key=api_key, dry_run=dry_run)
                     elif run.start["analyzer_type"] == "ses":
-                        ses_export(uid, api_key=api_key, dry_run=dry_run)
+                        export_ses_xps(uid, api_key=api_key, dry_run=dry_run)
                 elif run.start["scantype"] == "xas":
-                    xas_export(uid, api_key=api_key, dry_run=dry_run)
+                    export_xas(uid, api_key=api_key, dry_run=dry_run)
                 elif run.start["scantype"] == "resPES":
-                    resPES_export(uid, api_key=api_key, dry_run=dry_run)
+                    export_resPES(uid, api_key=api_key, dry_run=dry_run)
                 else:
-                    generic_export(uid, api_key=api_key, dry_run=dry_run)
+                    export_generic_1D(uid, api_key=api_key, dry_run=dry_run)
             else:
-                generic_export(uid, api_key=api_key, dry_run=dry_run)
+                export_generic_1D(uid, api_key=api_key, dry_run=dry_run)
     else:
         logger.info("Run was aborted, skipping exports")
-
-
-@flow
-def xas_export(uid, api_key=None, dry_run=False):
-    export_xas(uid, api_key=api_key, dry_run=dry_run)
-
-
-@flow
-def peak_export(uid, api_key=None, dry_run=False):
-    export_peak_xps(uid, api_key=api_key, dry_run=dry_run)
-
-
-@flow
-def generic_export(uid, api_key=None, dry_run=False):
-    export_generic_1D(uid, api_key=api_key, dry_run=dry_run)
-
-
-@flow
-def ses_export(uid, api_key=None, dry_run=False):
-    export_ses_xps(uid, api_key=api_key, dry_run=dry_run)
-
-
-@flow
-def resPES_export(uid, api_key=None, dry_run=False):
-    export_resPES(uid, api_key=api_key, dry_run=dry_run)
