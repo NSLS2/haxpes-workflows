72532a0e2be849608e900e90a4d3841e6713b272
Jun Aishima <jaishima@bnl.gov>
2026-05-12 15:51:32 -0400

    restore missing get_run() and get_api_key_from_env()
     * remove getting API key from end_of_run_workflow() to prevent passing
       API key in function calls

--- data_validation.py
+++ data_validation.py
@@ -1,8 +1,27 @@
+import os
 import time
 
 from prefect import flow, get_run_logger, task
+from tiled.client import from_uri
+from dotenv import load_dotenv
 
 from export_tools import get_run
+
+
+def get_api_key_from_env():
+    with open("/srv/container.secret", "r") as secrets:
+        load_dotenv(stream=secrets)
+    api_key = os.environ["TILED_API_KEY"]
+    return api_key
+
+
+@task(retries=2, retry_delay_seconds=10)
+def get_run(uid, api_key=None):
+    if not api_key:
+        api_key = get_api_key_from_env()
+    cl = from_uri("https://tiled.nsls2.bnl.gov", api_key=api_key)
+    run = cl[f"{BEAMLINE_OR_ENDSTATION}/raw"][uid]
+    return run
 
 
 @task(retries=2, retry_delay_seconds=10)
--- end_of_run_workflow.py
+++ end_of_run_workflow.py
@@ -86,8 +86,6 @@ def log_completion():
 @flow
 @slack
 def end_of_run_workflow(stop_doc, api_key=None, dry_run=False):
-    if api_key is None:
-        api_key = get_api_key_from_env()
     uid = stop_doc["run_start"]
     data_validation(uid, api_key=api_key)
     export_switchboard(uid, api_key=api_key, dry_run=dry_run)
